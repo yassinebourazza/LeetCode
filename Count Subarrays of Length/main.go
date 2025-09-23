8802993fec9c644c0926d9df8c61f58ae3e4cf2f
yassinebourazza <nite246135@gmail.com>
2025-09-23 10:03:43 +0100

    Solve LeetCode Problem : Count Subsarrays of Length

---
+++ Count Subarrays of Length/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(countSubarrays([]int{4, 5, 6}))
+	fmt.Println(countSubarrays([]int{0}))
+	fmt.Println(countSubarrays([]int{-1, -4 , -1,4}))
+}
+
+//CountSubsarrays x1 + x3 = x2 / 2 
+func countSubarrays(nums []int) int {
+	var count int
+	if len(nums) < 3 {
+		return count
+	}
+	for i:=2 ; i < len(nums) ;i++ {
+		if float64(nums[i]+nums[i-2]) == (float64(nums[i-1])/2) {
+			count++
+		}
+	}
+	return count
+}
